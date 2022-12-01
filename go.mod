c57024119c5ecdae0e0fb029d769086805839f2e
Rafael Macedo <macedo.rafaelfernandes@gmail.com>
2022-12-01 11:46:24 -0300

    connect to whatsapp

--- go.mod
+++ go.mod
@@ -1,3 +1,18 @@
 module github.com/macedo/whatsapp-rememberme
 
 go 1.19
+
+require (
+	github.com/mdp/qrterminal v1.0.1
+	go.mau.fi/whatsmeow v0.0.0-20221126173344-e660988acdbc
+)
+
+require (
+	filippo.io/edwards25519 v1.0.0 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/lib/pq v1.10.7 // indirect
+	go.mau.fi/libsignal v0.0.0-20221015105917-d970e7c3c9cf // indirect
+	golang.org/x/crypto v0.3.0 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
+	rsc.io/qr v0.2.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,64 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/lib/pq"
+	"github.com/mdp/qrterminal"
+	"go.mau.fi/whatsmeow"
+	"go.mau.fi/whatsmeow/store/sqlstore"
+	waLog "go.mau.fi/whatsmeow/util/log"
+)
+
+var client *whatsmeow.Client
+
+func init() {
+	sqlstore.PostgresArrayWrapper = pq.Array
+}
 
 func main() {
-	fmt.Println("hello world")
+	dbLog := waLog.Stdout("DATABASE", "INFO", true)
+	container, err := sqlstore.New("postgres", "postgres://postgres:0519@localhost:5432/whatsapp-rememberme", dbLog)
+	check_error(err)
+
+	deviceStore, err := container.GetFirstDevice()
+	check_error(err)
+
+	clientLog := waLog.Stdout("CLIENT", "INFO", true)
+	client = whatsmeow.NewClient(deviceStore, clientLog)
+
+	if client.Store.ID == nil {
+		qrChan, _ := client.GetQRChannel(context.Background())
+		err = client.Connect()
+		check_error(err)
+
+		for event := range qrChan {
+			if event.Event == "code" {
+				qrterminal.GenerateHalfBlock(event.Code, qrterminal.L, os.Stdout)
+				fmt.Println("QR code: ", event.Code)
+			} else {
+				fmt.Println("Login event: ", event.Event)
+			}
+		}
+	} else {
+		err = client.Connect()
+		check_error(err)
+	}
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	<-sigChan
+
+	client.Disconnect()
+}
+
+func check_error(err error) {
+	if err != nil {
+		fmt.Fprint(os.Stderr, err)
+		os.Exit(1)
+	}
 }
